4e58ee6adcb4c0a74eacadfe6fe40f36ac3d05b5
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2025-07-16 15:41:31 +0200

    Another commit

---
+++ src/main/java/org/example/downloader/DebianArchitecture.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2025 by Kristoffer Paulsson <kristoffer.paulsson@talenten.se>.
+ *
+ * This software is available under the terms of the MIT license. Parts are licensed
+ * under different terms if stated. The legal terms are attached to the LICENSE file
+ * and are made available on:
+ *
+ *      https://opensource.org/licenses/MIT
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Contributors:
+ *      Kristoffer Paulsson - initial implementation
+ */
+package org.example.downloader;
+
+public enum DebianArchitecture {
+    AMD_64("amd64"),
+    ARM_64("arm64"),
+    ARMEL("armel"),
+    ARMHF("armhf"),
+    I_386("i386"),
+    MIPS_64EL("mips64el"),
+    MIPS_EL("mipsel"),
+    PPC_64EL("ppc64el"),
+    RISC_V64("riscv64"),
+    S_390X("s390x"),
+    ALL("all");
+
+    final String arch;
+
+    DebianArchitecture(String arch) {
+        this.arch = arch;
+    }
+
+    String getArch() {
+        return this.arch;
+    }
+}
