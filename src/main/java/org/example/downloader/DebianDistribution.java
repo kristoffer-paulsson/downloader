8537db01d37e2c649a2c38d2d3c3dc81f48cf4ef
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2025-07-16 16:20:39 +0200

    Another commit

---
+++ src/main/java/org/example/downloader/DebianDistribution.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2025 by Kristoffer Paulsson <kristoffer.paulsson@talenten.se>.
+ *
+ * This software is available under the terms of the MIT license. Parts are licensed
+ * under different terms if stated. The legal terms are attached to the LICENSE file
+ * and are made available on:
+ *
+ *      https://opensource.org/licenses/MIT
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Contributors:
+ *      Kristoffer Paulsson - initial implementation
+ */
+package org.example.downloader;
+
+public enum DebianDistribution {
+    BOOKWORM("bookworm");
+
+    final String dist;
+
+    DebianDistribution(String distribution) {
+        this.dist = distribution;
+    }
+
+    String getComp() {
+        return this.dist;
+    }
+}
