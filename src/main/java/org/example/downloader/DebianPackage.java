15c8c669cdebe47c4bbbbaf1bb819f4f6f3b169c
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2025-07-17 16:19:00 +0200

    Another commit

--- src/main/java/org/example/downloader/DebianPackage.java
+++ src/main/java/org/example/downloader/DebianPackage.java
@@ -14,31 +14,10 @@
  */
 package org.example.downloader;
 
-
-
-public class DebianPackage {
-    public final String packageName;
-    public final String version;
-    public final String architecture;
-    public final String filename;
-    public final String sha256digest;
-    public final String distribution;
-
-    public DebianPackage(
-            String packageName,
-            String version,
-            String architecture,
-            String filename,
-            String sha256digest,
-            String distribution
-    ) {
-        this.packageName = packageName;
-        this.version = version;
-        this.architecture = architecture;
-        this.filename = filename;
-        this.sha256digest = sha256digest;
-        this.distribution = distribution;
-    }
+public record DebianPackage(
+        String packageName, String version, String architecture,
+        String filename, String sha256digest, String distribution
+) {
 
     public String buildDownloadUrl(String baseUrl) {
         return String.format("%s/%s", baseUrl, filename);
