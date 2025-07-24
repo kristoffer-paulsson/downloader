ee5c5c5d1913aa4a66c46edc27aaa70cf42a538b
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2025-07-24 18:48:14 +0200

    Adjustment

--- src/main/java/org/example/downloader/java/JavaEdition.java
+++ src/main/java/org/example/downloader/java/JavaEdition.java
@@ -2,9 +2,25 @@ package org.example.downloader.java;
 
 public enum JavaEdition {
     ORACLE("oracle"),
+    // https://www.oracle.com/java/technologies/javase/javase8u211-later-archive-downloads.html
+    // https://www.oracle.com/java/technologies/javase/jdk11-archive-downloads.html
+    // https://www.oracle.com/java/technologies/javase/jdk17-0-13-later-archive-downloads.html
+    // https://www.oracle.com/java/technologies/javase/jdk21-archive-downloads.html
+
     CORRETO("corretto"),
+    // https://docs.aws.amazon.com/corretto/latest/corretto-8-ug/downloads-list.html
+    // https://docs.aws.amazon.com/corretto/latest/corretto-11-ug/downloads-list.html
+    // https://docs.aws.amazon.com/corretto/latest/corretto-17-ug/downloads-list.html
+    // https://docs.aws.amazon.com/corretto/latest/corretto-21-ug/downloads-list.html
+
     ZULU("zulu"),
+    // https://cdn.azul.com/zulu/bin/
+
     TEMURIN("temurin");
+    // https://github.com/adoptium/temurin8-binaries/releases
+    // https://github.com/adoptium/temurin11-binaries/releases
+    // https://github.com/adoptium/temurin17-binaries/releases
+    // https://github.com/adoptium/temurin21-binaries/releases
 
     private final String edition;
 
--- src/main/java/org/example/downloader/java/JavaVersion.java
+++ src/main/java/org/example/downloader/java/JavaVersion.java
@@ -1,26 +1,19 @@
 package org.example.downloader.java;
 
 public enum JavaVersion {
-    JAVA_8("1.8.0_382", "https://download.oracle.com/java/8/archive/"),
-    JAVA_11("11.0.20", "https://download.oracle.com/java/11/archive/"),
-    JAVA_17("17.0.9", "https://download.oracle.com/java/17/archive/"),
-    JAVA_21("21.0.7", "https://download.oracle.com/java/21/archive/");
+    JAVA_8("1.8"),
+    JAVA_11("11"),
+    JAVA_17("17"),
+    JAVA_21("21");
 
 
     private final String version;
-    private final String baseUrl;
 
-    JavaVersion(String version, String baseUrl) {
+    JavaVersion(String version) {
         this.version = version;
-        this.baseUrl = baseUrl;
     }
 
     public String getVersion() {
         return version;
     }
-
-    public String getBaseUrl() {
-        return baseUrl;
-    }
-
 }
