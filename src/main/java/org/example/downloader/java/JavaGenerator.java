4bd39fab9e8c8c048d196f3a04737f58c463e850
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2025-07-27 16:33:40 +0200

    A little fix

--- src/main/java/org/example/downloader/java/JavaGenerator.java
+++ src/main/java/org/example/downloader/java/JavaGenerator.java
@@ -17,12 +17,10 @@ package org.example.downloader.java;
 import org.example.downloader.util.DownloadHelper;
 import org.example.downloader.util.Pair;
 
-import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -142,7 +140,6 @@ public class JavaGenerator {
 
     public static List<URL> generateCorrettoDownloadUrls() {
         List<URL> urls = new ArrayList<>();
-        // https://corretto.aws/[latest/latest_checksum]/amazon-corretto-[corretto_version]-[cpu_arch]-[os]-[package_type].[file_extension]
         String baseUrl = "https://corretto.aws/downlaods/latest/amazon-corretto-%s-%s-%s-%s.%s";
         for (JavaFlavor javaEdition : List.of(JavaFlavor.CORRETTO)) {
             for (JavaVersion javaVersion : correttoVersions) {
@@ -182,30 +179,25 @@ public class JavaGenerator {
         return urls;
     }
 
-    // https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u462-b08/OpenJDK8U-jre_aarch64_linux_hotspot_8u462b08.tar.gz
-    // https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u462-b08/OpenJDK8U-jdk_aarch64_linux_hotspot_8u462b08.tar.gz
-
-    // https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.28+6/OpenJDK11U-jre_aarch64_mac_hotspot_11.0.28_6.pkg.sig
-    // https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.28+6/OpenJDK11U-jdk_aarch64_linux_hotspot_11.0.28_6.tar.gz
-
-    // https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.9+9.1/OpenJDK17U-jre_x64_windows_hotspot_17.0.9_9.msi
-    // https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.9+9.1/OpenJDK17U-jdk_x64_windows_hotspot_17.0.9_9.zip
-
-    // https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.8+9/OpenJDK21U-jdk_x64_mac_hotspot_21.0.8_9.tar.gz
-    // https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.8+9/OpenJDK21U-jdk_aarch64_windows_hotspot_21.0.8_9.msi
-
     public static HashMap<JavaVersion, Pair<String, String>> temurinMinorPatch = new HashMap<>(Map.of(
             JavaVersion.JAVA_8, new Pair<>("u462-b08", "u462b08"),
             JavaVersion.JAVA_11, new Pair<>(".0.28+6", ".0.28_6"),
-            JavaVersion.JAVA_17, new Pair<>(".0.9+9.1", ".0.9_9"),
+            JavaVersion.JAVA_17, new Pair<>(".0.16+8", ".0.16_8"),
             JavaVersion.JAVA_21, new Pair<>(".0.8+9", ".0.8_9")
     ));
 
-    public static HashMap<JavaVersion, String> temurinTemplate = new HashMap<>(Map.of(
-            JavaVersion.JAVA_8, "",
-            JavaVersion.JAVA_11, "",
-            JavaVersion.JAVA_17, "",
-            JavaVersion.JAVA_21, ""
+    public static HashMap<JavaVersion, String> temurinJDKTemplate = new HashMap<>(Map.of(
+            JavaVersion.JAVA_8, "https://github.com/adoptium/temurin%s-binaries/releases/download/jdk%s%s/OpenJDK%sU-%s_%s_%s_hotspot_%s%s.%s",
+            JavaVersion.JAVA_11, "https://github.com/adoptium/temurin%s-binaries/releases/download/jdk-%s%s/OpenJDK%sU-%s_%s_%s_hotspot_%s%s.%s",
+            JavaVersion.JAVA_17, "https://github.com/adoptium/temurin%s-binaries/releases/download/jdk-%s%s/OpenJDK%sU-%s_%s_%s_hotspot_%s%s.%s",
+            JavaVersion.JAVA_21, "https://github.com/adoptium/temurin%s-binaries/releases/download/jdk-%s%s/OpenJDK%sU-%s_%s_%s_hotspot_%s%s.%s"
+    ));
+
+    public static HashMap<JavaVersion, String> temurinJRETemplate = new HashMap<>(Map.of(
+            JavaVersion.JAVA_8, "https://github.com/adoptium/temurin%s-binaries/releases/download/jdk%s%s/OpenJDK%sU-%s_%s_%s_hotspot_%s%s.%s",
+            JavaVersion.JAVA_11, "https://github.com/adoptium/temurin%s-binaries/releases/download/jdk-%s%s/OpenJDK%sU-%s_%s_%s_hotspot_%s%s.%s",
+            JavaVersion.JAVA_17, "https://github.com/adoptium/temurin%s-binaries/releases/download/jdk-%s%s/OpenJDK%sU-%s_%s_%s_hotspot_%s%s.%s",
+            JavaVersion.JAVA_21, "https://github.com/adoptium/temurin%s-binaries/releases/download/jdk-%s%s/OpenJDK%sU-%s_%s_%s_hotspot_%s%s.%s"
     ));
 
     public static HashMap<JavaPlatform, String> temurinPlatformsTranslation = new HashMap<>(
@@ -239,7 +231,7 @@ public class JavaGenerator {
                             new Pair<>(JavaArchitecture.X64, JavaPackage.ZIP),
                             new Pair<>(JavaArchitecture.X64, JavaPackage.MSI),
                             new Pair<>(JavaArchitecture.AARCH64, JavaPackage.ZIP),
-                            new Pair<>(JavaArchitecture.SPARC64, JavaPackage.MSI)
+                            new Pair<>(JavaArchitecture.AARCH64, JavaPackage.MSI)
                     ),
                     JavaPlatform.MACOS, List.of(
                             new Pair<>(JavaArchitecture.X64, JavaPackage.TAR_GZ),
@@ -253,12 +245,76 @@ public class JavaGenerator {
             )
     );
 
-    // -----------------------------------------------------------------------------------------
+    public static List<URL> generateTemurinDownloadUrls() {
+        List<URL> urls = new ArrayList<>();
+        // https://corretto.aws/[latest/latest_checksum]/amazon-corretto-[corretto_version]-[cpu_arch]-[os]-[package_type].[file_extension]
+        for (JavaFlavor javaEdition : List.of(JavaFlavor.TEMURIN)) {
+            for (JavaVersion javaVersion : temurinVersions) {
+                for (JavaType javaType : temurinTypes) {
+                    for (Map.Entry<JavaPlatform, List<Pair<JavaArchitecture, JavaPackage>>> entry : temurinPlatforms.entrySet()) {
+                        JavaPlatform javaPlatform = entry.getKey();
+                        for (Pair<JavaArchitecture, JavaPackage> pair : entry.getValue()) {
+                            JavaArchitecture javaArchitecture = pair.getFirst();
+                            JavaPackage javaPackage = pair.getSecond();
 
-    // https://cdn.azul.com/zulu/bin/zulu21.44.17-ca-jdk21.0.8-linux.x86_64.rpm
-    // https://cdn.azul.com/zulu/bin/zulu17.60.17-ca-jdk17.0.16-win_x64.msi
-    // https://cdn.azul.com/zulu/bin/zulu11.82.19-ca-jdk11.0.28-macosx_aarch64.dmg
-    // https://cdn.azul.com/zulu/bin/zulu8.88.0.19-ca-jdk8.0.462-linux_x64.zip
+                            String baseUrl = "";
+                            if (javaType == JavaType.JDK) {
+                                baseUrl = temurinJDKTemplate.get(javaVersion);
+                            } else if (javaType == JavaType.JRE) {
+                                baseUrl = temurinJRETemplate.get(javaVersion);
+                            }
+
+                            if(javaVersion != JavaVersion.JAVA_21 && javaPlatform == JavaPlatform.WINDOWS && javaArchitecture == JavaArchitecture.AARCH64) {
+                                continue; // No AARCH64 support for Windows in Temurin 8, 11, and 17
+                            } else if (javaPlatform == JavaPlatform.WINDOWS && javaArchitecture == JavaArchitecture.SPARC64) {
+                                continue; // No SPARC64 support for Windows in Temurin
+                            } else if((javaVersion == JavaVersion.JAVA_8 || javaVersion == JavaVersion.JAVA_11) && javaPlatform == JavaPlatform.LINUX && javaArchitecture == JavaArchitecture.RISCV64) {
+                                continue; // No RISC-V support for Linux in Temurin 8 and 11
+                            } else if(javaVersion == JavaVersion.JAVA_8 && javaPlatform == JavaPlatform.MACOS && javaArchitecture == JavaArchitecture.AARCH64) {
+                                continue; // No AARCH64 support for macOS in Temurin 8
+                            } else if (javaVersion == JavaVersion.JAVA_8 && javaPlatform == JavaPlatform.LINUX && (javaArchitecture == JavaArchitecture.S390X)) {
+                                continue;
+                            }
+
+                            String version = javaVersion.getVersion();
+                            String type = javaType.getType();
+                            String platform = javaPlatform.getOs();
+                            String architecture = javaArchitecture.getArch();
+                            String packageType = javaPackage.getPackageType();
+
+                            if(javaPlatform == JavaPlatform.MACOS) {
+                                platform = "mac"; // For Temurin, macOS platform is prefixed with mac
+                            }
+
+                            String url = String.format(
+                                    baseUrl,
+                                    version,
+                                    version,
+                                    temurinMinorPatch.get(javaVersion).getFirst(),
+                                    version,
+                                    type,
+                                    architecture,
+                                    platform,
+                                    version,
+                                    temurinMinorPatch.get(javaVersion).getSecond(),
+                                    packageType
+                            );
+                            try {
+                                urls.add(new URI(url).toURL());
+                            } catch (MalformedURLException e) {
+                                throw new RuntimeException(e);
+                            } catch (URISyntaxException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return urls;
+    }
+
+    // -----------------------------------------------------------------------------------------
 
     public static HashMap<JavaVersion, Pair<String, String>> zuluMinorPatch = new HashMap<>(Map.of(
             JavaVersion.JAVA_8, new Pair<>(".88.0.19", ".0.462"),
@@ -371,9 +427,10 @@ public class JavaGenerator {
         List<List<URL>> allUrls = new ArrayList<>();
         allUrls.add(generateOracleDownloadUrls());
         allUrls.add(generateCorrettoDownloadUrls());
+        allUrls.add(generateTemurinDownloadUrls());
         allUrls.add(generateZuluDownloadUrls());
 
-        for (URL url : allUrls.get(1)) {
+        for (URL url : allUrls.get(2)) {
             try {
                 long size = DownloadHelper.queryUrlFileDownloadSizeWithRedirect(url);
                 System.out.println(size + ": " + url);
