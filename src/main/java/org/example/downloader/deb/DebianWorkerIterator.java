a44d8399c5629fe972dbc6ba1eb70881c095dc4a
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2025-08-08 18:19:55 +0200

    A little fix

--- src/main/java/org/example/downloader/deb/DebianWorkerIterator.java
+++ src/main/java/org/example/downloader/deb/DebianWorkerIterator.java
@@ -14,42 +14,131 @@
  */
 package org.example.downloader.deb;
 
-import org.example.downloader.DebianMirrorCache;
-import org.example.downloader.DebianPackage;
-import org.example.downloader.DebianWorker;
-import org.example.downloader.util.InversionOfControl;
+import org.example.downloader.GeneralEnvironment;
+import org.example.downloader.WorkLogger;
+import org.example.downloader.util.*;
 
-import java.util.Iterator;
-import java.util.List;
-import java.util.NoSuchElementException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
 
-public class DebianWorkerIterator implements Iterator<DebianWorker> {
-    private final Iterator<org.example.downloader.DebianPackage> packages;
-    private final InversionOfControl ioc;
-    private final org.example.downloader.DebianMirrorCache mirrorCache;
+public class DebianWorkerIterator extends WorkerIterator<DebianPackage> {
 
+    private final DebianDownloadEnvironment dde;
+    private final Iterator<DebianPackage> packageIterator;
+    private final WorkLogger logger;
+    private final BlockChainHelper.Blockchain chain;
+    private final DebianMirrorCache mirrors;
 
-    DebianWorkerIterator(InversionOfControl ioc, List<DebianPackage> packages) {
-        this.ioc = ioc;
-        this.mirrorCache = ioc.resolve(DebianMirrorCache.class);
-        this.packages = packages.iterator();
+    private final AtomicReference<List<DownloadHelper.Download>> incompleteDownloads = new AtomicReference<>(new ArrayList<>());
+
+    public DebianWorkerIterator(
+            GeneralEnvironment ge,
+            DebianDownloadEnvironment dde,
+            HashMap<String, DebianPackage> packages,
+            BlockChainHelper.Blockchain chain,
+            WorkLogger logger
+    ) {
+        this.dde = dde;
+        this.packageIterator = packages.values().iterator();
+        this.chain = chain;
+        this.logger = logger;
+
+        this.mirrors = new DebianMirrorCache(ge);
+        this.mirrors.loadCachedMirrors(false);
+    }
+
+    @Override
+    protected DebianWorkerIterator.DebianWorker createWorker() {
+        DebianPackage pkg = packageIterator.next();
+        try {
+            String baseUrl = mirrors.getNextMirror();
+            URL downloadURL = URI.create(pkg.buildDownloadUrl(baseUrl)).toURL();
+            Path downloadPath = pkg.buildSavePath(dde);
+
+            return new DebianWorkerIterator.DebianWorker(
+                    pkg,
+                    new DownloadHelper.Download(
+                            downloadURL,
+                            downloadPath
+                    ),
+                    chain,
+                    mirrors,
+                    logger,
+                    baseUrl
+            );
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public boolean hasNext() {
-        return packages.hasNext();
+        return packageIterator.hasNext();
     }
 
-    @Override
-    public DebianWorker next() {
-        if (!hasNext()) {
-            throw new NoSuchElementException("No more items in the package");
+    public List<DownloadHelper.Download> getIncompleteDownloads() {
+        return incompleteDownloads.get();
+    }
+
+    public class DebianWorker extends Worker<DebianPackage> {
+
+        private final BlockChainHelper.Blockchain chain;
+        private final DebianMirrorCache mirrors;
+        private final String baseUrl;
+
+        public DebianWorker(
+                DebianPackage basePackage,
+                DownloadHelper.Download downloadTask,
+                BlockChainHelper.Blockchain chain,
+                DebianMirrorCache mirrors,
+                WorkLogger logger,
+                String baseUrl
+        ) {
+            super(basePackage, downloadTask, logger);
+            this.chain = chain;
+            this.mirrors = mirrors;
+            this.baseUrl = baseUrl;
         }
-        return new DebianWorker(packages.next(), ioc);
-    }
 
-    @Override
-    public void remove() {
-        throw new UnsupportedOperationException("Remove operation is not supported");
+        @Override
+        protected void doWhenDownloadVerifiedSuccessful() throws IOException {
+            if(chain != null) {
+                chain.addRow(basePackage.uniqueKey(), basePackage.getFilename(), basePackage.getSha256Digest());
+                logger.warning("Download of " + basePackage.uniqueKey() + " registered to blockchain.");
+            }
+        }
+
+        @Override
+        protected void doWhenDownloadVerifiedFailure() throws IOException {
+            Files.deleteIfExists(downloadTask.getFilePath());
+            logger.warning("File of download " + basePackage.uniqueKey() + " deleted due to failed verification.");
+        }
+
+        @Override
+        protected void doWhenTimedOut() throws IOException {
+            incompleteDownloads.get().add(downloadTask);
+            mirrors.reportBadMirror(baseUrl);
+            System.out.println("Download timed out for " + basePackage.uniqueKey());
+        }
+
+        @Override
+        protected void doWhenError() throws IOException {
+            incompleteDownloads.get().add(downloadTask);
+            mirrors.reportBadMirror(baseUrl);
+            System.out.println("Download errored for " + basePackage.uniqueKey());
+        }
+
+        @Override
+        protected void doWhenUnexpected() throws IOException {
+            incompleteDownloads.get().add(downloadTask);
+            mirrors.reportBadMirror(baseUrl);
+            System.out.println("Something unexpected for " + basePackage.uniqueKey());
+        }
     }
 }
