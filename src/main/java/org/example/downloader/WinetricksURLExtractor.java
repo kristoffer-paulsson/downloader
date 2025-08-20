886aae15e8e97c6aad9d3632e1e6101ac22988ad
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2025-08-20 15:31:12 +0200

    A little fix

--- src/main/java/org/example/downloader/WinetricksURLExtractor.java
+++ src/main/java/org/example/downloader/WinetricksURLExtractor.java
@@ -36,7 +36,9 @@ public class WinetricksURLExtractor {
 
     private static final String[] SKIP = {
             "47113b285253a1ebce04527a31d734c0dfce5724e8d2643c6c1b822a940e7073",
-            "bf4ee47d0df1870104f4fada8a68c2fb29e94fea9284c7bb6a6b385a718d8a18" // utorrent
+            "bf4ee47d0df1870104f4fada8a68c2fb29e94fea9284c7bb6a6b385a718d8a18", // utorrent
+            "630c86a202c40cbcd430701977d4f1fefa6151624ef9a4870040dff45e547dea", // server2020
+            "0f0cc11f000593a064d419462a8467b529fed8075b21a605a40785baa3d2f611", // aka.ms
     };
 
     private static final String WINRAR_URL = "https://www.win-rar.com/fileadmin/winrar-versions";
