31e457b32e3d20e5c2c09049f94ab145480d7985
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2025-08-20 15:38:48 +0200

    A little fix

--- src/main/java/org/example/downloader/WinetricksURLExtractor.java
+++ src/main/java/org/example/downloader/WinetricksURLExtractor.java
@@ -37,7 +37,6 @@ public class WinetricksURLExtractor {
     private static final String[] SKIP = {
             "47113b285253a1ebce04527a31d734c0dfce5724e8d2643c6c1b822a940e7073",
             "630c86a202c40cbcd430701977d4f1fefa6151624ef9a4870040dff45e547dea", // server2020
-            "0f0cc11f000593a064d419462a8467b529fed8075b21a605a40785baa3d2f611", // aka.ms
     };
 
     private static final String WINRAR_URL = "https://www.win-rar.com/fileadmin/winrar-versions";
@@ -174,7 +173,7 @@ public class WinetricksURLExtractor {
     private static void wDownloadHandler(List<String> fields) {
         //System.out.println(String.join(" ", fields));
         if(fields.size() < 3) {
-            System.out.println(PrintHelper.coloredMessage(String.join(" ", fields), PrintHelper.ANSI_MAGENTA));
+            System.out.println(PrintHelper.coloredMessage("Missing: " + String.join(" ", fields), PrintHelper.ANSI_MAGENTA));
             return;
         }
         URL url = extractURL(fields.get(1));
