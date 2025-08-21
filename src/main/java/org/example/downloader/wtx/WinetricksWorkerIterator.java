22e3802a9f009ec59cb8a383f4b3913184cc40c0
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2025-08-21 14:06:34 +0200

    Just fix

--- src/main/java/org/example/downloader/wtx/WinetricksWorkerIterator.java
+++ src/main/java/org/example/downloader/wtx/WinetricksWorkerIterator.java
@@ -64,7 +64,7 @@ public class WinetricksWorkerIterator extends WorkerIterator<WinetricksPackage> 
     protected WinetricksWorker createWorker() {
         WinetricksPackage pkg = packageIterator.next();
         try {
-            Path downloadPath = Path.of(String.format("%s/%s", wde.getDownloadDir(), pkg.getFilename()));
+            Path downloadPath = wde.getDownloadDir().resolve(pkg.getCategory()).resolve(pkg.getFilename());
             return new WinetricksWorker(pkg, new DownloadHelper.Download(pkg.getRealUrl(), downloadPath), chain, logger);
         } catch (MalformedURLException | URISyntaxException e) {
             throw new RuntimeException(e);
